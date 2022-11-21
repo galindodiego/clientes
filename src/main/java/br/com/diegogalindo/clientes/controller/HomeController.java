15e776f1e4a76f9e473b57e68a58978b009ca74c
Diego Galindo <diegogalindo84@gmail.com>
2022-11-21 20:17:39 -0300

    ViewController

--- src/main/java/br/com/diegogalindo/clientes/controller/HomeController.java
+++ src/main/java/br/com/diegogalindo/clientes/controller/HomeController.java
@@ -1,8 +1,13 @@
 package br.com.diegogalindo.clientes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -12,5 +17,27 @@ public class HomeController {
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/mensagem")
+    public ModelAndView mensagem(ModelMap modelMap ){
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("mensagem");
+        modelAndView.addObject("mensagem","Outra Mensagem vinda do servidor");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/saudacao")
+    public ModelAndView saudacao(@RequestParam(required = false, defaultValue ="Default Galindo" ) String nome){
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("saudacao");
+        modelAndView.addObject("nome", nome);
+
+        return modelAndView;
+
     }
 }
