8a8f79c4402c772da6f4f6cb3f9413dd00169cca
Diego Galindo <diegogalindo84@gmail.com>
2022-11-23 10:27:10 -0300

    View casdatro cliente criada

--- src/main/java/br/com/diegogalindo/clientes/controller/ClienteController.java
+++ src/main/java/br/com/diegogalindo/clientes/controller/ClienteController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
 import br.com.diegogalindo.clientes.model.Cliente;
 import br.com.diegogalindo.clientes.repository.ClienteRepository;
 
@@ -47,6 +49,15 @@ public class ClienteController {
 
         clienteRepository.deleteById(id);
         
+        return modelAndView;
+    }
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadastrar(){
+        ModelAndView modelAndView = new ModelAndView("cliente/cadastro");
+        
+        modelAndView.addObject("cliente", new Cliente());
+
         return modelAndView;
     }
 }
