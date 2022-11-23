c2b507e2dac36f4486b6d01a55d2134cebed41c7
Diego Galindo <diegogalindo84@gmail.com>
2022-11-23 10:38:48 -0300

    Action cadastro cliente em funcionamento

--- src/main/java/br/com/diegogalindo/clientes/model/Cliente.java
+++ src/main/java/br/com/diegogalindo/clientes/model/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.diegogalindo.clientes.model;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -8,6 +9,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 @Entity
 @Table
@@ -21,7 +25,8 @@ public class Cliente {
     private String nome;
 
     @Column(nullable = false, name = "data_nascimento")
-    private Date dataNascimento;
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDate dataNascimento;
 
 
     @Column(nullable = false)
@@ -32,7 +37,7 @@ public class Cliente {
     }
 
 
-    public Cliente(Long id, String nome, Date dataNascimento, String profissao) {
+    public Cliente(Long id, String nome, LocalDate dataNascimento, String profissao) {
         this.id = id;
         this.nome = nome;
         this.dataNascimento = dataNascimento;
@@ -60,12 +65,12 @@ public class Cliente {
     }
 
 
-    public Date getDataNascimento() {
+    public LocalDate getDataNascimento() {
         return dataNascimento;
     }
 
 
-    public void setDataNascimento(Date dataNascimento) {
+    public void setDataNascimento(LocalDate dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
 
